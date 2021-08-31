4830f1648777813ef9527b2e729276b5dfcbac25
Leszek Taratuta <ata6502@hotmail.com>
2021-08-30 20:31:34 -0400

    Add HelloWorld example

---
+++ HelloWorld/App.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "App.xaml.g.h"
+
+namespace winrt::HelloWorld::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
+        void OnNavigationFailed(IInspectable const&, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
+    };
+}
