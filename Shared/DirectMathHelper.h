615d9fa71442e1f22e3f4f44d70cab6f8c81c37d
Leszek Taratuta <ata6502@hotmail.com>
2022-02-09 21:03:47 -0500

    Add debug functions for XMMATRIX and XMVECTOR

--- Shared/DirectMathHelper.h
+++ Shared/DirectMathHelper.h
@@ -19,3 +19,37 @@ inline DirectX::XMFLOAT3 VectorToFloat3(const DirectX::FXMVECTOR& v)
     XMStoreFloat3(&f, v);
     return f;
 }
+
+#if defined(_DEBUG)
+// Example: DX::DebugTrace(L"num = %4.2f\n", num);
+inline void DebugTrace(const wchar_t* format, ...)
+{
+    // Generate the message string.
+    va_list args;
+    va_start(args, format); // initialize the argument list
+    wchar_t buffer[1024];
+    ASSERT(_vsnwprintf_s(buffer, _countof(buffer) - 1, format, args) != -1);
+    va_end(args);
+
+    OutputDebugStringW(buffer); // this is a Windows function
+}
+
+inline void OutputDebug(DirectX::CXMMATRIX matrix)
+{
+    DirectX::XMFLOAT4X4 m;
+    XMStoreFloat4x4(&m, matrix);
+    DebugTrace(L"%2.2f %2.2f %2.2f %2.2f\n", m._11, m._12, m._13, m._14);
+    DebugTrace(L"%2.2f %2.2f %2.2f %2.2f\n", m._21, m._22, m._23, m._24);
+    DebugTrace(L"%2.2f %2.2f %2.2f %2.2f\n", m._31, m._32, m._33, m._34);
+    DebugTrace(L"%2.2f %2.2f %2.2f %2.2f\n", m._41, m._42, m._43, m._44);
+    DebugTrace(L"\n");
+}
+
+inline void OutputDebug(DirectX::FXMVECTOR vector)
+{
+    DirectX::XMFLOAT4 v;
+    XMStoreFloat4(&v, vector);
+    DebugTrace(L"%2.2f %2.2f %2.2f %2.2f\n", v.x, v.y, v.z, v.w);
+    DebugTrace(L"\n");
+}
+#endif
