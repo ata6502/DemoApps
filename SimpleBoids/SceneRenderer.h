fac9a2c1da3559954a8b2a16980a0edd2e5841ac
Leszek Taratuta <ata6502@hotmail.com>
2024-07-25 21:45:26 -0400

    Extract cbuffers to a separate file

--- SimpleBoids/SceneRenderer.h
+++ SimpleBoids/SceneRenderer.h
@@ -1,48 +1,8 @@
 #pragma once
 
+#include "ConstantBuffers.h"
 #include "DeviceResources.h"
 #include "TextureMeshGenerator.h"
-
-struct DirectionalLightDesc
-{
-    DirectX::XMFLOAT4 Ambient;
-    DirectX::XMFLOAT4 Diffuse;
-    DirectX::XMFLOAT4 Specular;
-    DirectX::XMFLOAT3 Direction;
-    float Pad;
-};
-
-struct MaterialDesc
-{
-    DirectX::XMFLOAT4 Ambient;
-    DirectX::XMFLOAT4 Diffuse;
-    DirectX::XMFLOAT4 Specular; // w = SpecularPower
-};
-
-struct CBufferNeverChanges
-{
-    DirectionalLightDesc Light;
-};
-
-struct CBufferOnResize
-{
-    DirectX::XMFLOAT4X4 Projection;
-};
-
-struct CBufferPerFrame
-{
-    DirectX::XMFLOAT4X4 View;
-    DirectX::XMFLOAT3 EyePosition;
-    float Pad;
-};
-
-struct CBufferPerObject
-{
-    DirectX::XMFLOAT4X4 World;
-    DirectX::XMFLOAT4X4 WorldInvTranspose;
-    MaterialDesc Material;
-    DirectX::XMFLOAT4X4 TextureTransform;
-};
 
 class SceneRenderer
 {
