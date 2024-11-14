22fd9018165a3f71adde23b234895e70bdf36b5d
Leszek Taratuta <ata6502@hotmail.com>
2024-11-13 21:39:28 -0500

    Add an empty project for shadow mapping

---
+++ ShadowMapping/MainPage.cpp
@@ -0,0 +1,10 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::ShadowMapping::implementation
+{
+}
---
+++ ShadowMapping/MainPage.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::ShadowMapping::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage()
+        {
+            // Xaml objects should not call InitializeComponent during construction.
+            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+        }
+    };
+}
+
+namespace winrt::ShadowMapping::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
